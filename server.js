f1b39dd41d1900c00ecfea0344fc213c545977df
Chris O'Mara <comara17@gmail.com>
2017-08-17 18:05:20 -0400

    changed server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+var db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -26,5 +28,8 @@ var routes = require("./controllers/pizza_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
-console.log("Server listening on: " + port);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
